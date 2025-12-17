2f14111887d493ca31e81e59ba662d3c9da2618d
thanvika05 <thanvika.b2024aiml@sece.ac.in>
2025-12-17 10:13:06 +0530

    first

---
+++ frontend/project/src/App.jsx
@@ -0,0 +1,61 @@
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Navbar from "./assets/Navbar";
+import Home from "./Home";
+import Products from "./assets/Products";
+import Contact from "./Contact";
+import Cart from "./Cart";
+import Login from "./Login";
+import Register from "./Register";
+import ProtectedRoute from "./ProtectedRoute";
+
+
+function App() {
+  const [cart, setCart] = useState([]);
+  const [isLoggedIn, setIsLoggedIn]= useState(!!localStorage.getItem("token")
+);
+
+  // useEffect --- watch cart changes
+  useEffect(() => {
+    console.log("Cart Updated:", cart);
+  }, [cart]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  return (
+    <>
+      <Navbar cartCount={cart.length}
+      isLoggedIn = {isLoggedIn}
+      on Logout ={() => {
+        localStorage.removeItem("token");
+        handletLoginState(false);
+      }} />
+
+      <Routes>
+        <Route path="/login" element={<Login onLogin={() => handletLoginState(true)} />} />
+        <Route path="/register" element={<Register onRegister={() => handletLoginState(true)} />} />
+        <Route
+          path="/products"
+          element={
+            <ProtectedRoute>
+              <Products addToCart={addToCart} />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+            path = "/cart"
+            element = {
+              <ProtectedRoute>
+                <Cart cart={cart} />
+              </ProtectedRoute>
+            }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/project/src/Home.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"; // Add this import
+import storeImage from './assetsone/chrisitems.jpg';
+export default function Home() {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>Welcome to Our Store</h1>
+            <p>Discover our wide range of products and enjoy shopping with us!</p>
+            {/* image only */}
+
+            <Link to="/Products">
+                <img
+                    src={storeImage} // Add your image URL here
+                    alt="Store Items"
+                    style={{ width: '100%', maxWidth: '600px', height: 'auto', marginTop: '20px' }}
+                />
+                <p>Click the image to explore products</p>
+            </Link>
+        </div>
+    );
+}
